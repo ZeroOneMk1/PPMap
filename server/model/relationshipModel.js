729ee358540f02eacb0b37c130e56f0126d9e81f
Azura Simsek <yusuftariksimsek@gmail.com>
2026-01-22 18:14:16 -0500

    moved functions to RelationshipController because I realized all relationship actions need auth

--- server/model/relationshipModel.js
+++ server/model/relationshipModel.js
@@ -4,7 +4,8 @@ const relationshipSchema = new mongoose.Schema({
     UUID: { type: String, required: true, unique: true },
     romantic: { type: Boolean, required: true },
     sexual: { type: Boolean, required: true },
-    persons: { type: [String], required: true, minlength: 2, maxlength: 2 }
+    persons: { type: [String], required: true, minlength: 2, maxlength: 2 },
+    time_created: { type: Date, default: Date.now }
 })
 
 export default mongoose.model("Relationship", relationshipSchema)
