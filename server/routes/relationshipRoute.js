ee07e655e90d61bc1969c10f2ed4ab8935bc4606
Azura Simsek <yusuftariksimsek@gmail.com>
2026-01-22 19:23:16 -0500

    added endpoint for getting related persons

--- server/routes/relationshipRoute.js
+++ server/routes/relationshipRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createRelationshipAsPerson, joinRelationshipAsPerson, endRelationshipAsPerson, editRelationshipAsPerson, deleteAllInvalidRelationships } from "../controller/relationshipController.js"
+import { createRelationshipAsPerson, joinRelationshipAsPerson, endRelationshipAsPerson, editRelationshipAsPerson, deleteAllInvalidRelationships, getRelatedPersons } from "../controller/relationshipController.js"
 
 const relationshipsRouter = express.Router();
 
@@ -8,4 +8,5 @@ relationshipsRouter.post("/relationship/join", joinRelationshipAsPerson);
 relationshipsRouter.post("/relationship/end", endRelationshipAsPerson);
 relationshipsRouter.post("/relationship/edit", editRelationshipAsPerson);
 relationshipsRouter.post("/relationship/deleteinvalid", deleteAllInvalidRelationships);
+relationshipsRouter.get("/relationship/getrelatedpersons", getRelatedPersons);
 export default relationshipsRouter;
